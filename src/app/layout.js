f5941fc5230ea773fc12c389ea868810dfe09253
harsshhhgit <harshbsi567@gmail.com>
2023-10-02 12:58:30 +0530

    favicons changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,32 +3,28 @@ import clsx from 'clsx'
 import { Circle2, Circle1 } from '@/components/Circles'
 import { openSans, inter, sohne, fira } from './fonts/fonts'
 import Header from '@/components/Header'
-import Head from 'next/head'
 
 export const metadata = {
   title: { default: 'Harshbardhan Singh', template: '%s — harsshhh' },
   creator: 'Harshbardhan Singh',
   publisher: 'Harshbardhan Singh',
   description: 'Software Engineer | Student',
+  keywords: ['Harshbardhan Singh', 'harsshhhgit', 'harsshhh'],
+  authors: [{ name: 'Harshbardhan Singh', url: 'https://harsshhh.xyz' }],
+  colorScheme: 'dark',
   icons: {
-    icon: '/favicon.png',
-    shortcut: '/favicon.png',
+    icon: '/favicons/favicon.png',
+    shortcut: '/favicons/shortcut.png',
+    apple: [
+      { url: '/favicons/apple-touch-icon.png' }
+    ]
   },
+  category: 'technology',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={clsx(openSans.variable, inter.variable, sohne.variable, fira.variable)}>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
-        <link rel="manifest" href="favicon/site.webmanifest" />
-        <link rel="shortcut icon" href="favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="msapplication-config" content="favicon/browserconfig.xml" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       {/* <body className={openSans.className}>{children}</body> */}
       <body >
         <Circle1 />
