422901a6b4c83ad345e7d850e89cd3e72f8690e3
harsshhhgit <harshbsi567@gmail.com>
2023-10-06 22:05:30 +0530

    contentlayer configured for markdown files

---
+++ contentlayer.config.js
@@ -0,0 +1,47 @@
+import {
+    defineDocumentType,
+    makeSource,
+} from 'contentlayer/source-files'
+import remarkGfm from 'remark-gfm'
+import remarkSectionize from 'remark-sectionize'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypePrism from 'rehype-prism-plus'
+import rehypePresetMinify from 'rehype-preset-minify'
+
+const computedFields = {
+    slug: {
+        type: 'string',
+        resolve: (doc) =>
+            `/post/${doc._raw.sourceFileName.replace(/\.mdx$/, '')}`,
+    },
+}
+
+const Post = defineDocumentType(() => ({
+    name: 'Post',
+    filePathPattern: '**/*.mdx',
+    contentType: 'mdx',
+    fields: {
+        title: { type: 'string', required: true },
+        description: { type: 'string', required: true },
+        publishedAt: { type: 'string', required: true },
+        isPublished: { type: 'boolean', required: true }
+    },
+    computedFields,
+}))
+
+export default makeSource({
+    contentDirPath: 'posts',
+    documentTypes: [Post],
+    mdx: {
+        remarkPlugins: [remarkGfm, remarkSectionize],
+        rehypePlugins: [
+            rehypeSlug,
+            rehypeCodeTitles,
+            rehypePrism,
+            rehypePresetMinify,
+            [rehypeAutolinkHeadings, { properties: { className: ['anchor'] } }],
+        ],
+    },
+})
