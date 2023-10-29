6050ca24b112191313ef01b55ddf6b514a6c7e34
harsshhhgit <harshbsi567@gmail.com>
2023-10-29 10:35:56 +0530

    Gitignore modified and MDX added

---
+++ src/components/MDX.js
@@ -0,0 +1,19 @@
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import Link from 'next/link'
+import Image, { ImageProps } from 'next/image'
+
+const PostImage = (props) => (
+    <Image
+        {...(props)}
+        draggable={false}
+    />
+)
+
+const components = { PostImage }
+
+const MDX = ({ code }) => {
+    const Component = useMDXComponent(code)
+    return <Component components={{ ...components }} />
+}
+
+export default MDX;
