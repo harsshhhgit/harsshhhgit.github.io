f658f43d6aa7f67851d137a0ed5d48ae12ab99bc
harsshhhgit <harshbsi567@gmail.com>
2023-10-30 22:33:29 +0530

    minor change

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,7 +2,7 @@ import { allPosts } from 'contentlayer/generated';
 
 export default async function sitemap() {
     const blogs = allPosts.map((post) => ({
-        url: `https://www.harsshhh.com/post/${post.slug}`,
+        url: `https://www.harsshhh.com${post.slug}`,
         lastModified: post.publishedAt,
     }));
 
